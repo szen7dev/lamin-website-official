4b5f61f7601a942f0a1fa51d2a2f0bbfbdaaa402
dikans <kdnguyen2711@gmail.com>
2025-04-19 15:16:07 +0700

    fix height measure history page

--- app/(protected)/account/height-measure-history/page.tsx
+++ app/(protected)/account/height-measure-history/page.tsx
@@ -215,7 +215,9 @@ export default function HeightMeasureHistoryPage() {
                         <div>{item.note}</div>
                         <Link
                           className="text-primary text-sm flex items-center hover:underline"
-                          href={`/tai-khoan/lich-su-do-cao/${item.id}?name=${encodeURIComponent(item.name)}`}>
+                          href={`/tai-khoan/lich-su-do-cao/${item.id}?name=${encodeURIComponent(
+                            item.name,
+                          )}`}>
                           Xem chi tiết{' '}
                           <ChevronNoArrowIcon className="w-4 h-3" />
                         </Link>
