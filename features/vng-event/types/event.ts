66521e934137155ce2fef13e577b4dbc87ad0a56
dikans <kdnguyen2711@gmail.com>
2025-05-27 16:35:11 +0700

    Add api for event page

---
+++ features/vng-event/types/event.ts
@@ -0,0 +1,19 @@
+export interface Event {
+  _id: string;
+  income: number;
+  expense: number;
+  images: any[];
+  status: number;
+  state: number;
+  company: string;
+  userCreate: string;
+  date: string;
+  address: string;
+  name: string;
+  note: string;
+  namecv: string;
+  modifyAt: string;
+  createAt: string;
+  __v: number;
+  userUpdate: string;
+}
