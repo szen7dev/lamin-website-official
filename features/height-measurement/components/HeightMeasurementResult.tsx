18e7367a9c4d5111e4a8c82d63b27797dee8ad95
szen7dev <app.szen7@gmail.com>
2026-06-26 17:52:38 +0700

    fix

--- features/height-measurement/components/HeightMeasurementResult.tsx
+++ features/height-measurement/components/HeightMeasurementResult.tsx
@@ -724,7 +724,7 @@ export default function HeightMeasurementResult({
             những thói quen sinh hoạt xấu
           </p>
           <p className="border-b border-grayscale-100 text-grayscale-90">
-            • Con có cơ hội tăng trưởng 7-15cm so với dự đoán khi thay đổi được các thói quen Dinh dưỡng - Giấc ngủ - Vận động một cách tích cực
+            • Con có cơ hội tăng trưởng 7-15cm so với dự đoán khi thay đổi được các thói quen Dinh dưỡng - Giấc ngủ - Vận động - Vi chất một cách tích cực
           </p>
           <ul
             aria-label="Khuyến nghị"
